fd612d4f9a3d3b7fa4157b0946dd914b5de3002e
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-29 13:28:42 -0700

    refactor for spring profiles

--- src/test/java/com/galvanize/services/OrderServiceTest.java
+++ src/test/java/com/galvanize/services/OrderServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class OrderServiceTest {
 
     @Autowired // means use the real thing
