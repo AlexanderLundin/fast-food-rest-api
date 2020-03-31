8dc64f3451968699cf32d09eabf3ada2d2ac3fe8
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-31 03:54:44 -0700

    front-end image spinner

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -4,14 +4,25 @@ import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 import { Button, Container } from 'reactstrap';
 
+import Logo from './logo.svg';
+
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            speed: 50
+        }
+    }
+
     render() {
         return (
             <div>
                 <AppNavbar/>
                 <Container fluid>
                     <Button color="link"><Link to="/orders">Orders View</Link></Button>
+                    <img style={{animation: `App-logo-spin ${this.state.speed}s linear infinite`}} src={Logo} alt="logo.svg"/>
                 </Container>
+
             </div>
         );
     }
