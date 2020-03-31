7ed83ee3a6dc9a996651e2d50e834c5094cffdbd
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-30 22:53:32 -0700

    restructure into front-end and server

--- front-end/src/OrderList.js
+++ front-end/src/OrderList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Button, ButtonGroup, Container, Table } from 'reactstrap';
 import AppNavbar from './AppNavbar';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 class OrderList extends Component {
@@ -15,27 +15,7 @@ class OrderList extends Component {
     componentDidMount() {
         this.setState({isLoading: true});
 
-        // const httpHeaders = {'Accept-Charset' : 'utf-8', 'Origin' : 'http://localhost:8080' };
-        // const myHeaders = new Headers(httpHeaders);
-        //
-        //
-        // const myRequest = new Request('http://fastfoodrestapi-env.eba-mz87ac4w.us-east-2.elasticbeanstalk.com/api/orders', {
-        //     method: 'GET',
-        //     headers: myHeaders,
-        //     mode: 'cors',
-        //     cache: 'default',
-        // });
-        //
-        // fetch(myRequest)
-        //         .then(response => response.json())
-        //         .then(data =>
-        //             this.setState({orders: data, isLoading: false})
-        //         );
 
-        // fetch('http://fastfoodrestapi-env.eba-mz87ac4w.us-east-2.elasticbeanstalk.com/api/orders',
-        //     { method: 'get', mode: 'no-cors', })
-        //     .then(() => { console.log('Works!');
-        //     });
 
         fetch('http://fastfoodrestapi-env.eba-mz87ac4w.us-east-2.elasticbeanstalk.com/api/orders')
             .then(response => response.json())
@@ -64,33 +44,33 @@ class OrderList extends Component {
             return <p>Loading...</p>;
         }
         console.log(orders);
-        // const orderList = orders.map(order => {
-        //
-        //     return <tr key={order.id}>
-        //         <td style={{whiteSpace: 'nowrap'}}>{order.id}</td>
-        //         <td>{order.customerName}</td>
-        //         <td>{order.createdAt}</td>
-        //         <td>{order.status}</td>
-        //         <td>{order.description}</td>
-        //         <td>{order.note}</td>
-        //         <td>{order.lastUpdated}</td>
-        //         <td>
-        //             <ButtonGroup>
-        //                 {/*<Button size="sm" color="primary" tag={Link} to={"/api/orders/" + order.id}>Edit</Button>*/}
-        //                 <Button size="sm" color="danger" onClick={() => this.remove(order.id)}>Delete</Button>
-        //             </ButtonGroup>
-        //         </td>
-        //     </tr>
-        // });
+        const orderList = orders.map(order => {
+
+            return <tr key={order.id}>
+                <td style={{whiteSpace: 'nowrap'}}>{order.id}</td>
+                <td>{order.customerName}</td>
+                <td>{order.createdAt}</td>
+                <td>{order.status}</td>
+                <td>{order.description}</td>
+                <td>{order.note}</td>
+                <td>{order.lastUpdated}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" tag={Link} to={"/api/orders/" + order.id}>Edit</Button>
+                        <Button size="sm" color="danger" onClick={() => this.remove(order.id)}>Delete</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
 
         return (
             <div>
                 <AppNavbar/>
                 <Container fluid>
                     <div className="float-right">
-                        {/*<Button color="success" tag={Link} to="/orders">Add Order</Button>*/}
+                        <Button color="success" tag={Link} to="/orders">Add Order</Button>
                     </div>
-                    <h3>Customer Service Request Table</h3>
+                    <h3>Order Request Table</h3>
                     <Table className="mt-4">
                         <thead>
                         <tr>
@@ -103,9 +83,9 @@ class OrderList extends Component {
                             <th width="10%">Updated Date</th>
                         </tr>
                         </thead>
-                        {/*<tbody>*/}
-                        {/*{orderList}*/}
-                        {/*</tbody>*/}
+                        <tbody>
+                        {orderList}
+                        </tbody>
                     </Table>
                 </Container>
             </div>
